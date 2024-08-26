81879cf7e3a4adf28e537fbd5b5c5c3730bf6696
Kajalpandey230 <12212019it@gmail.com>
2024-08-26 21:26:25 +0530

    initial

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.0",
+    defaultNetwork: "goerli",
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: "https://rpc.ankr.com/eth_goerli",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
