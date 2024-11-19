31a341ae979383af47d8b62c559887b8ffe5c19f
themillenniumfalcon <nishankpr2002@gmail.com>
2024-11-19 22:00:55 +0530

    changed some things

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { Admin } from "./Admin";
+export { Card } from "./Card";
+export { ConnectButton } from "./ConnectButton";
+export { Game } from "./Game";
+export { Header } from "./Header";
+export { Main } from "./Main";
+export { Notifications } from "./Notifications";
+export { Warning } from "./Warning";
